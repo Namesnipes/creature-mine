4f692011729ba1529ddfd6d980e6d78323a566cc
Red <saraalva@usc.edu>
2023-07-06 21:29:24 -0700

    ookie2

--- src/ClickComponent.ts
+++ src/ClickComponent.ts
@@ -17,7 +17,6 @@ export class ClickComponent extends Component {
         owner.on("pointertap", this.onClick, this);
     }
 
-<<<<<<< Updated upstream
     public onClick(e: FederatedMouseEvent):void{
         if(!this.mClicked){
             const particleContainer = new Container();
@@ -30,54 +29,6 @@ export class ClickComponent extends Component {
                 emitter.emit = false
             },500);
             
-=======
-    public onClick(e: FederatedMouseEvent): void {
-        if (!this.mClicked) {
-            const particleContainer = new ParticleContainer();
-            this.mOwner.addChild(particleContainer);
-
-            const emitter = new particles.Emitter(particleContainer, {
-                lifetime: { min: 0.1, max: 1 },
-                frequency: 0.01,
-                spawnChance: 1,
-                particlesPerWave: 1,
-                emitterLifetime: 120,
-                maxParticles: 10,
-                pos: { x: 327, y: 200 },
-                autoUpdate: true,
-                behaviors: [
-                    {
-                        type: 'scale',
-                        config: {
-                            scale: {
-                                list: [
-                                    {
-                                        value: 0.2,
-                                        time: 0
-                                    },
-                                    {
-                                        value: 0.1,
-                                        time: 1
-                                    }
-                                ],
-                            },
-                        }
-                    },
-                    {
-                        type: 'spawnShape',
-                        config: { type: 'torus', data: { x: 0, y: 0, radius: 50 } },
-                    },
-                    { type: 'textureSingle', config: { texture: "cookie.png" } },
-                ],
-            });
-            emitter.autoUpdate = true; // If you keep it false, you have to update your particles yourself.
-            emitter.updateSpawnPos(e.globalX - Manager.width / 2, e.globalY - Manager.height / 2);
-            emitter.emit = true;
-            setTimeout(function () {
-                emitter.emit = false;
-            }, 500);
-
->>>>>>> Stashed changes
             this.mClicked = true;
             this.mClickNum++;
         }
