6ad33aacb7740256036bfc0b33f3ece70e23f1ee
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-07-17 16:36:19 -0700

    Add linting configuration

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "semi": [
+            "error",
+            "always"
+        ],
+        'func-style': [
+            'error',
+            'declaration',
+            {
+              // Define a custom regex pattern to enforce PascalCase for function names.
+              pattern: /^[A-Z][A-Za-z0-9]*$/,
+            },
+        ]
+    }
+};
