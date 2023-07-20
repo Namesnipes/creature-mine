d7fa717b6043ff722d7d66d7d251b3f68ff525c5
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-07-19 22:09:44 -0700

    Fixed linter dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-unused-vars": "off",
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+		"no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": "off",
-        "semi": [
-            "error",
-            "always"
-        ],
-        '@typescript-eslint/naming-convention': [
-            'error',
-            {
-              selector: 'function',
-              format: ['PascalCase'],
-            },
-          ]
-    }
+		"semi": [
+			"error",
+			"always"
+		]
+	}
 };
--- src/components/LerpComponent.ts
+++ src/components/LerpComponent.ts
@@ -4,53 +4,53 @@ import { Component } from "./Component";
 
 export class LerpComponent extends Component {
 
-    private mLerpPercent: number = 0;
-    private isLerping: boolean = false;
-    private originX: number;
-    private originY: number;
-    private lerpToX: number;
-    private lerpToY: number;
-    private lerpPromiseResolve: (value: void | PromiseLike<void>) => void;
+	private mLerpPercent: number = 0;
+	private isLerping: boolean = false;
+	private originX: number;
+	private originY: number;
+	private lerpToX: number;
+	private lerpToY: number;
+	private lerpPromiseResolve: (value: void | PromiseLike<void>) => void;
 
-    constructor(owner: Actor) {
-        super(owner);
-        this.mOwner = owner;
-    }
+	constructor(owner: Actor) {
+		super(owner);
+		this.mOwner = owner;
+	}
 
-    //TODO: add speed as a parameter to lerp
-    //TODO: dont leak the promise resolver
-    /**
+	//TODO: add speed as a parameter to lerp
+	//TODO: dont leak the promise resolver
+	/**
      * Uses LERP to move the actor to the specified coordinates.
      *
      * @param {number} x - The x-coordinate to move to.
      * @param {number} y - The y-coordinate to move to.
      * @return {Promise<void>} A Promise that resolves when the object has finished moving.
      */
-    public Move(x: number, y: number): Promise<void> {
-        return new Promise((resolve, reject) => {
-            this.isLerping = true;
-            this.originX = this.mOwner.x;
-            this.originY = this.mOwner.y;
-            this.lerpToX = x;
-            this.lerpToY = y;
-            this.mLerpPercent = 0;
-            this.lerpPromiseResolve = resolve;
-        });
-    }
-    public override Update(delta: number): void {
-        if (this.isLerping) {
-            //console.log(Helper.Lerp(this.mOwner.x, this.lerpToX, this.mLerpPercent),this.mLerpPercent)
-            this.mOwner.x = Helper.Lerp(this.originX, this.lerpToX, this.mLerpPercent);
-            this.mOwner.y = Helper.Lerp(this.originY, this.lerpToY, this.mLerpPercent);
+	public Move(x: number, y: number): Promise<void> {
+		return new Promise((resolve, reject) => {
+			this.isLerping = true;
+			this.originX = this.mOwner.x;
+			this.originY = this.mOwner.y;
+			this.lerpToX = x;
+			this.lerpToY = y;
+			this.mLerpPercent = 0;
+			this.lerpPromiseResolve = resolve;
+		});
+	}
+	public override Update(delta: number): void {
+		if (this.isLerping) {
+			//console.log(Helper.Lerp(this.mOwner.x, this.lerpToX, this.mLerpPercent),this.mLerpPercent)
+			this.mOwner.x = Helper.Lerp(this.originX, this.lerpToX, this.mLerpPercent);
+			this.mOwner.y = Helper.Lerp(this.originY, this.lerpToY, this.mLerpPercent);
 
-            this.mLerpPercent += delta / 1000; // percent is between 0 and 1
-            if (this.mLerpPercent >= 1) {
-                this.lerpPromiseResolve();
-                this.isLerping = false;
-            }
-        }
-    }
+			this.mLerpPercent += delta / 1000; // percent is between 0 and 1
+			if (this.mLerpPercent >= 1) {
+				this.lerpPromiseResolve();
+				this.isLerping = false;
+			}
+		}
+	}
 
-    public override ProcessInput(): void {
-    }
+	public override ProcessInput(): void {
+	}
 }
