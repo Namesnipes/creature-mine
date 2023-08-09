f716b12fb383f1a41998b7871f91dcc565e76161
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-08-08 22:24:20 -0700

    Format Jar

--- src/actors/Jar.ts
+++ src/actors/Jar.ts
@@ -7,40 +7,40 @@ export enum JarType{
     Blueberry
 }
 export class Jar extends InventoryItem{
-    mJarType: JarType;
-    mSprite: string;
-    constructor(scene: Scene, jarType: JarType) {
+	mJarType: JarType;
+	mSprite: string;
+	constructor(scene: Scene, jarType: JarType) {
 		super(scene, ItemType.Jar);
-        this.mJarType = jarType;
-        switch(this.mJarType){
-            case JarType.Regular:
-            {
-                this.mSprite = "jar1.png";
-                break;
-            }
-            case JarType.Strawberry:
-            {
-                this.mSprite = "jar2.png";
-                break;
-            }
-            case JarType.Blueberry:
-            {
-                this.mSprite = "jar3.png";
-                break;
-            }
-        }
-        this.MakeNewInventoryItem(this.mSprite);
-        //add item to correct inventory list in ui
-        this.mScene.GetUI().AddJarToInventory(this);
+		this.mJarType = jarType;
+		switch(this.mJarType){
+		case JarType.Regular:
+		{
+			this.mSprite = "jar1.png";
+			break;
+		}
+		case JarType.Strawberry:
+		{
+			this.mSprite = "jar2.png";
+			break;
+		}
+		case JarType.Blueberry:
+		{
+			this.mSprite = "jar3.png";
+			break;
+		}
+		}
+		this.MakeNewInventoryItem(this.mSprite);
+		//add item to correct inventory list in ui
+		this.mScene.GetUI().AddJarToInventory(this);
 	}
-    public override AddItem(): void{
-        if(this.mNumItems == this.MAX_STACK){
-            //make new jar item if current stack is full
-            this.mIsFull = true;
-            new Jar(this.mScene, this.mJarType);
-        }
-        else{
-            super.AddItem();
-        }
-    }
+	public override AddItem(): void{
+		if(this.mNumItems == this.MAX_STACK){
+			//make new jar item if current stack is full
+			this.mIsFull = true;
+			new Jar(this.mScene, this.mJarType);
+		}
+		else{
+			super.AddItem();
+		}
+	}
 }
